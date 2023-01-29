0e18ff27582ebed4509218505f78a2a567755207
Lucas Gallo <100171791+LucasGallo182@users.noreply.github.com>
2023-01-29 04:41:29 -0300

    Modificacion header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-                Seguimiento Pacientes {''} {/* agrego espacio en blanco */}
+                Seguimiento Pacientes {''} con Github {/* agrego espacio en blanco */}
                 <span className="text-indigo-600">Veterinaria</span>
             </h1>
     )
